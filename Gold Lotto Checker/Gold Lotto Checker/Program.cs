8384e5e931223c6a5c355ac549e06ee51d2892ac
BrendanDonc <46376758+BrendanDonc@users.noreply.github.com>
2019-01-27 12:45:44 +1000

    Add files via upload

---
+++ Gold Lotto Checker/Gold Lotto Checker/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+
+namespace Gold_Lotto_Checker {
+
+    ///<summary>
+    ///
+    ///Program which takes pre-input arrays of lotto numbers
+    ///and drawn numbers then displays all numbers in the
+    ///console as well as how many winning and supplementary
+    ///numbers each lotto array has within.
+    ///
+    ///Author Brendan Doncaster n9819363 April 2017
+    ///
+    ///</summary>
+
+    class Program {
+
+        static void Main() {
+
+            int[,] lottoNumbers = {
+                                  { 4, 7, 19, 23, 28, 36},
+                                  {14, 18, 26, 34, 38, 45},
+                                  { 8, 10,11, 19, 28, 30},
+                                  {15, 17, 19, 24, 43, 44},
+                                  {10, 27, 29, 30, 32, 41},
+                                  { 9, 13, 26, 32, 37,  43},
+                                  { 1, 3, 25, 27, 35, 41},
+                                  { 7, 9, 17, 26, 28, 44},
+                                  {17, 18, 20, 28, 33, 38}
+                              };
+
+            int[] drawNumbers = new int[] { 44, 9, 17, 43, 26, 7, 28, 19 };
+            int games;
+
+            Welcome();
+            games = GetNumGames(lottoNumbers);       
+            PrintGames(lottoNumbers, games);
+            PrintDraw(drawNumbers);
+            PrintResults(games, drawNumbers, lottoNumbers);
+
+            ExitProgram();
+        }//end Main
+
+        /// <summary>
+        /// 
+        /// Gets the number of lotto games from the list of lotto numbers
+        /// 
+        /// </summary>
+        /// <param name="lottoNumbers">List of lotto numbers input into code</param>
+        /// <returns>Number of different games within lottoNumbers</returns>
+
+        static int GetNumGames(int[,] lottoNumbers) {
+            int games = lottoNumbers.GetUpperBound(0);
+            return games;
+        }//end GetNumGames
+
+        /// <summary>
+        /// 
+        /// Displays welcome message
+        /// 
+        /// </summary>
+
+        static void Welcome() {
+            Console.WriteLine("\n\tWelcome to Lotto Checker\n\n");
+        }//end Welcome
+
+        /// <summary>
+        /// 
+        /// Prints out the lotto numbers with a new line per game
+        /// 
+        /// </summary>
+        /// <param name="lottoNumbers">List of lotto numbers input into code</param>
+        /// <param name="games">Number of different games within lottoNumbers</param>
+
+        static void PrintGames(int[,] lottoNumbers, int games) {
+            Console.Write("\nYour Lotto numbers are");
+            for (int i = 0; i <= games; i++) {
+                Console.Write("\nGame   "+(i+1) + ":");
+                for (int j = 0; j <= 5; j++) {
+                    if(lottoNumbers[i,j] >= 10) {
+                        Console.Write("  " + lottoNumbers[i,j]);
+                    } else {
+                        Console.Write("   " + lottoNumbers[i,j]);
+                    }
+                }
+                Console.Write("\n");
+            }
+        }//end PrintGames
+
+        /// <summary>
+        /// 
+        /// Prints out the drawn numbers
+        /// 
+        /// </summary>
+        /// <param name="drawNumbers">List of winning and supplementary numbers</param>
+
+        static void PrintDraw(int[] drawNumbers) {
+            Console.Write("\n\n\n Lotto Draw Numbers are\n\n ");
+            for (int i = 0; i <= 7; i++) {
+                if (drawNumbers[i] >= 10) {
+                            Console.Write("  " + drawNumbers[i]);
+                } else {
+                    Console.Write("   " + drawNumbers[i]);
+                }
+            }
+        }//end PrintDraw
+
+        /// <summary>
+        /// 
+        /// Prints the amount of winning numbers and supplementary numbers for each game
+        /// 
+        /// </summary>
+        /// <param name="games">Number of different games within lottoNumbers</param>
+        /// <param name="drawNumbers">List of winning and supplementary numbers</param>
+        /// <param name="lottoNumbers">List of lotto numbers input into code</param>
+
+        static void PrintResults(int games, int[] drawNumbers, int[,] lottoNumbers) {
+            int winNum;
+            int suppNum;
+
+
+            for (int i = 0; i <= games; i++) {
+                winNum = FindWinners(i, drawNumbers, lottoNumbers);
+                suppNum = FindSupps(i, drawNumbers, lottoNumbers);
+                Console.WriteLine("\n\n\tfound " + winNum + " winning numbers and "
+                    + suppNum + " supplementary numbers in game " + (i+1) + "\n");
+            }
+        }//end PrintResults
+
+        /// <summary>
+        /// 
+        /// Gets the number of winning numbers for the current game
+        /// 
+        /// </summary>
+        /// <param name="game">Current game</param>
+        /// <param name="drawNumbers">List of winning and supplementary numbers</param>
+        /// <param name="lottoNumbers">List of lotto numbers input into code</param>
+        /// <returns>Number of winning numbers</returns>
+
+        static int FindWinners(int game, int[] drawNumbers, int[,] lottoNumbers) {
+            int wins = 0;
+            for (int i = 0; i <= 5; i++) {
+                for (int j = 0; j <= 5; j++) {
+                    if (lottoNumbers[game,i] == drawNumbers[j]) {
+                        wins = wins + 1;
+                    }
+                }
+            }
+            return wins;
+        }//end FindWinners
+
+        /// <summary>
+        /// 
+        /// Gets the number of supplementary numbers for the current game
+        /// 
+        /// </summary>
+        /// <param name="game">Current game</param>
+        /// <param name="drawNumbers">List of winning and supplementary numbers</param>
+        /// <param name="lottoNumbers">List of lotto numbers input into code</param>
+        /// <returns>Number of supplementary numbers</returns>
+
+        static int FindSupps(int game, int[] drawNumbers, int[,] lottoNumbers) {
+            int supps = 0;
+            for (int i = 0; i <= 5; i++) {
+                for (int j = 6; j <= 7; j++) {
+                    if (lottoNumbers[game,i] == drawNumbers[j]) {
+                        supps = supps + 1;
+                    }
+                }
+            }
+            return supps;
+        }//end FindSupps
+
+
+        /// <summary>
+        /// 
+        /// Displays the exit message and exits program on any key
+        /// 
+        /// </summary>
+
+        static void ExitProgram() {
+            Console.WriteLine("\n\n\t Thanks for using Lotto Checker");
+            Console.Write("\n\nPress any key to exit program: ");
+            Console.ReadKey();
+        }//end ExitProgram
+
+
+
+    }//end class
+}//end namespace
---
+++ WTH/WTH/WTH.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WTH {
+    /// <summary>
+    /// 
+    /// Menu driven program which calculates
+    /// the waist-height ratio and gives
+    /// the degree of risk for cardiovascular 
+    /// disease based on the given gender.
+    /// 
+    /// Author Brendan Doncaster March 2017
+    /// 
+    /// 
+    /// </summary>
+    class WTH {
+        static void Main(string[] args) {
+
+        int waist;
+        int height;
+        int gender;
+        double ratio;
+        bool recalculate = true;
+
+        WelcomeMessage();
+
+        while (recalculate == true) {
+        waist = ReadWaist();
+        height = ReadHeight();
+        GenderMenu();
+        gender = ReadOption();
+        ratio = PerformRatio(waist, height);
+        PerformRisk(gender, ratio);
+        recalculate = Recalc();
+        }
+
+        ExitProgram();
+        }//end Main
+
+
+        /// <summary>
+        /// 
+        /// Displays the greeting 
+        /// message/title for the program.
+        /// 
+        /// </summary>
+        static void WelcomeMessage() {
+        Console.WriteLine("\n\tWelcome to Waist to Height Ratio Calculator\n");
+        }//end WelcomeMessage
+
+
+        /// <summary>
+        /// 
+        /// Asks the user for their
+        /// waist measurement (cm) to be input.
+        /// Also checks for measurement's validity.
+        /// 
+        /// </summary>
+        /// <returns> Waist measurement</returns>
+        static int ReadWaist() {
+        string input;
+        int waist;
+        bool isInt;
+        bool okayInput;
+
+        //Asks for an input and checks if it is a number that is at least 60
+        do {
+        Console.Write("\nEnter waist measurement in cm: ");
+        input = Console.ReadLine();
+        isInt = int.TryParse(input, out waist);
+        if (!isInt) {
+        okayInput = false;
+        }
+        else if (waist < 60) {
+        okayInput = false;
+        }
+        else {
+        okayInput = true;
+        }
+        if (!okayInput) {
+        Console.WriteLine("You did not enter a valid measurement.\n\n Please try again");
+        }
+        } while (!okayInput);
+
+        return waist;
+        }//end ReadWaist
+
+
+        /// <summary>
+        /// 
+        /// Asks the user for their
+        /// height measurement (cm) to be input.
+        /// Also checks for measurement's validity.
+        /// 
+        /// </summary>
+        /// <returns> Height measurement</returns>
+        static int ReadHeight() {
+        string input;
+        int height;
+        bool isInt;
+        bool okayInput;
+
+        //Asks for an input and checks if it is a number that is at least 120
+        do {
+        Console.Write("\nEnter height measurement in cm: ");
+        input = Console.ReadLine();
+        isInt = int.TryParse(input, out height);
+        if (!isInt) {
+        okayInput = false;
+        }
+        else if (height < 120) {
+        okayInput = false;
+        }
+        else {
+        okayInput = true;
+        }
+        if (!okayInput) {
+        Console.WriteLine("\n\n\tmeasurement must be at least 120cm");
+        }
+        } while (!okayInput);
+
+        return height;
+
+        }//end ReadHeight
+
+
+        /// <summary>
+        /// 
+        /// Displays a menu option for the
+        /// user to input their gender.
+        /// 
+        /// </summary>
+        static void GenderMenu() {
+        string menu = "\nAre you"
+                    + "\n\t1> male"
+                    + "\n\t2> female"
+                    + "\n\n\tEnter your option <1 or 2>: ";
+
+        Console.Write(menu);
+        } //end GenderMenu
+
+
+        /// <summary>
+        /// 
+        /// Reads the gender option chosen during
+        /// GenderMenu and checks response for 
+        /// validity. Re-runs GenderMenu if 
+        /// validity check is failed.
+        /// 
+        /// </summary>
+        /// <returns>Gender choice as 1 or 2 if response was valid.</returns>
+        static int ReadOption() {
+        string choice;
+        int option;
+        bool isInt;
+        bool okayChoice;
+
+        //Checks if user input is a number and if it is either '1' or '2'
+        do {
+        choice = Console.ReadLine();
+        isInt = int.TryParse(choice, out option);
+
+        if (!isInt) {
+        okayChoice = false;
+        }
+        else if (option != 1 && option != 2) {
+        okayChoice = false;
+        }
+        else {
+        okayChoice = true;
+        }
+
+        if (!okayChoice) {
+        Console.WriteLine("You did not enter a correct option.\n\n Please try again");
+        GenderMenu();
+        }
+        } while (!okayChoice);
+
+        return option;
+        } //end ReadOption
+
+
+        /// <summary>
+        /// 
+        /// Calculates the waist to height ratio
+        /// based off of supplied measurements.
+        /// 
+        /// </summary>
+        /// <param name="waist">Waist measurement supplied by user</param>
+        /// <param name="height">Height measurement supplied by user</param>
+        /// <returns>Calculated waist to height ratio</returns>
+        static double PerformRatio(int waist, int height) {
+        double waistDouble = waist;
+        double heightDouble = height;
+        double ratio = (waistDouble / heightDouble);
+        ratio = Math.Round(ratio, 3);
+        return ratio;
+        }//end PerformRatio
+
+
+        /// <summary>
+        /// 
+        ///Uses defined risk threshholds per
+        ///gender to assess the degree of risk for 
+        ///cardiovascular disease.
+        ///
+        /// </summary>
+        /// <param name="gender">Gender representation, 
+        ///1 being for male and 2 being for female.</param>
+        /// <param name="ratio">Waist to heigh ratio</param>
+        static void PerformRisk(int gender, double ratio) {
+        if (gender == 1) {
+        if (ratio < 0.536) {
+        Console.WriteLine("\nYour waist to height ratio is " + ratio
+            + "\n\tand\n you are at a low risk of developing obesity related cardiovascular diseases.");
+        }
+        else {
+        Console.WriteLine("\nYour waist to height ratio is " + ratio
+            + "\n\tand\n you are at a high risk of developing obesity related cardiovascular diseases.");
+        }
+        }
+        else if (gender == 2) {
+        if (ratio < 0.492) {
+        Console.WriteLine("\nYour waist to height ratio is " + ratio
+            + "\n\tand\n you are at a low risk of developing obesity related cardiovascular diseases.");
+        }
+        else {
+        Console.WriteLine("\nYour waist to height ratio is " + ratio
+            + "\n\tand\n you are at a high risk of developing obesity related cardiovascular diseases.");
+        }
+        }
+        else {
+        Console.WriteLine("\nError calculating risk. \n\n Please try again");
+        }
+        }//end PerformRisk
+
+
+        /// <summary>
+        /// 
+        /// Asks the user if they wish to run the
+        /// calculation process again.
+        /// 
+        /// </summary>
+        /// <returns>True if user wishes to rerun the program</returns>
+        static bool Recalc() {
+        string choice;
+        Console.Write("\n\nAnother calculation <Enter Y or N>:");
+        choice = Console.ReadLine();
+        if (choice == "Y" || choice == "y") {
+        return true;
+        }
+        else {
+        return false;
+        }
+
+        }//end Recalc
+
+
+        /// <summary>
+        /// 
+        /// Terminates the program gracefully with a press any key.
+        /// 
+        /// </summary>
+        static void ExitProgram() {
+        Console.Write("\n\nPress any key to terminate program ...");
+        Console.ReadKey();
+        }//end ExitProgram
+    }
+}
